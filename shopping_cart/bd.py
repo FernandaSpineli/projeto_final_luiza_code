ba2116edc6b7937eb21e7d0a2ad7eaaac4e7d16a
Taila Musardo <102119229+tailamusardo@users.noreply.github.com>
2022-10-12 23:52:44 -0300

    update url bd2

--- shopping_cart/bd.py
+++ shopping_cart/bd.py
@@ -10,7 +10,7 @@ def iniciar_cliente_mongo() -> AsyncIOMotorClient:
     cliente_mongo = AsyncIOMotorClient()
     return cliente_mongo
 
-cliente_mongo = iniciar_cliente_mongo(mongodb+srv://polifrenhan:dbpass@cluster0.h9yu1ce.mongodb.net/code-mesa-banho)
+cliente_mongo = iniciar_cliente_mongo(mongodb+srv://polifrenhan:dbpass@cluster0.h9yu1ce.mongodb.net/magalu-JA)
 
 def obter_base_dados() -> AsyncIOMotorDatabase:
     return cliente_mongo.get_default_database()
